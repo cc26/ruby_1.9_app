3f9c92fc2dd28c5d9e40a8d215014adf01be7df5
cc26 <reynold8002@gmail.com>
2014-10-24 13:57:41 -0700

    Add user following without test

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,19 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
+
+
   has_many :microposts, dependent: :destroy
+
+  # this follows that
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+
+  # that follows this
+  has_many :reverse_relationships, foreign_key: "followed_id",  dependent: :destroy,  class_name: 'Relationship'
+  has_many :followers, through: :reverse_relationships, source: :follower
+
+
 
   before_save { |user| user.email = email.downcase }
   before_save  :create_remember_token
@@ -27,10 +39,20 @@ class User < ActiveRecord::Base
   after_validation {self.errors.messages.delete(:password_digest)}
 
   def feed
-    # This is preliminary. See "Following users" for the full implementation.
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
   end
 
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    self.relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
+  end
 
   private
     def create_remember_token
